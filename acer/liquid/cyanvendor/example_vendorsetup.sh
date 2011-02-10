861533416cf3dc838722a863c1f38c80b4a81711
koudelka <koudelkaa@gmail.com>
2011-02-10 11:14:53 +0100

    update cyanogenvendor files, add README

---
+++ acer/liquid/cyanvendor/example_vendorsetup.sh
@@ -0,0 +1,24 @@
+add_lunch_combo cyanogen_ace-eng
+add_lunch_combo cyanogen_bravo-eng
+add_lunch_combo cyanogen_bravoc-eng
+add_lunch_combo cyanogen_crespo-eng
+add_lunch_combo cyanogen_buzz-eng
+#add_lunch_combo cyanogen_dream_sapphire-eng
+add_lunch_combo cyanogen_espresso-eng
+add_lunch_combo cyanogen_generic-eng
+add_lunch_combo cyanogen_glacier-eng
+#add_lunch_combo cyanogen_harmony-eng
+add_lunch_combo cyanogen_hero-eng
+#add_lunch_combo cyanogen_heroc-eng
+add_lunch_combo cyanogen_inc-eng
+add_lunch_combo cyanogen_legend-eng
+add_lunch_combo cyanogen_liberty-eng
+add_lunch_combo cyanogen_one-eng
+add_lunch_combo cyanogen_passion-eng
+add_lunch_combo cyanogen_sholes-eng
+add_lunch_combo cyanogen_supersonic-eng
+add_lunch_combo cyanogen_vision-eng
+add_lunch_combo cyanogen_z71-eng
+add_lunch_combo cyanogen_liquid-eng
+
+PATH=$PATH:$PWD/vendor/cyanogen/tools ; export PATH
